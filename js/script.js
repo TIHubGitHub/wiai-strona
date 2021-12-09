7b76994d8684e2e559d2d1b94cdf99e643a4c7f9
TIHub-JSanchoG <84404230+TIHubGitHub@users.noreply.github.com>
2021-12-09 12:29:10 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+// Przekierowywanie ze zdjęcia na stronę główną
 const navlogo = document.getElementById("nav-logo")
-
 navlogo.addEventListener("click", () => {
     window.location.href = "../";
 })
+
+// Data z copyright
+const fdate = document.getElementById("fdate");
+let date =  new Date().getFullYear();
+fdate.innerText = date;
